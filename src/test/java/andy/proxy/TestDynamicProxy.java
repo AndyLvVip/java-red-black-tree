6e24941ff3ea0c8d971fcae00f13d41f875242a4
andy.lv <lvhs@ucacc.com>
2018-02-05 09:35:55 +0800

    dynamic proxy...

--- src/test/java/andy/proxy/TestDynamicProxy.java
+++ src/test/java/andy/proxy/TestDynamicProxy.java
@@ -3,6 +3,9 @@ package andy.proxy;
 import org.junit.Test;
 
 import java.lang.reflect.Proxy;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * @Author: andy.lv
@@ -16,5 +19,82 @@ public class TestDynamicProxy {
         PayInvocationHandler handler = new PayInvocationHandler(new PayImpl());
         Pay pay = (Pay) Proxy.newProxyInstance(Pay.class.getClassLoader(), new Class[]{Pay.class}, handler);
         pay.pay(1.23);
+    }
+
+    public boolean foo(char c) {
+        System.out.print(c);
+        return true;
+    }
+
+    private static class Foo {
+        private String name;
+
+        public Foo(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == null)
+                return false;
+            if(obj instanceof Foo)
+                return Objects.equals(this.name, ((Foo) obj).getName());
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(name);
+        }
+    }
+
+    @Test
+    public void testEquals() {
+        Set<Foo> foos = new HashSet<>();
+        foos.add(new Foo("hello"));
+        foos.add(new Foo(new String("hello")));
+        System.out.println(foos.size());
+    }
+
+    @Test
+    public void length() {
+        Set set = new HashSet();
+        set.add("A1");
+        set.add("A2");
+
+        set.add(new String("A1"));
+        set.add(new String("A2"));
+        System.out.println("A1" == new String("A1"));
+        System.out.println("A1".equals(new String("A1")));
+        System.out.println(set.size());
+    }
+
+    @Test
+    public void doFoo() {
+        int i = 0;
+
+        for (foo('A'); foo('B') && (i < 2); foo('C')) {
+            i++;
+            foo('D');
+        }
+    }
+
+    public void changeStr(String str){
+        str = "Welcome To Midea";
+    }
+
+    @Test
+    public void change() {
+        String str = "Welcome";
+        changeStr(str);
+        System.out.println(str);
     }
 }
